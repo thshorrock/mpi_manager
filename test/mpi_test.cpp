1e544a510e20ff7c7b2823ea7b0112860429550d
Tom Shorrock <t.h.shorrock@gmail.com>
2011-07-07 11:18:37 +0100

    added license and improved guards

--- test/mpi_test.cpp
+++ test/mpi_test.cpp
@@ -1,4 +1,28 @@
 
+/***********************************************************************************
+ ***********************************************************************************
+ **                                                                               **
+ **  Copyright (C) 2011 Tom Shorrock <t.h.shorrock@gmail.com> 
+ **                                                                               **
+ **                                                                               **
+ **  This program is free software; you can redistribute it and/or                **
+ **  modify it under the terms of the GNU General Public License                  **
+ **  as published by the Free Software Foundation; either version 2               **
+ **  of the License, or (at your option) any later version.                       **
+ **                                                                               **
+ **  This program is distributed in the hope that it will be useful,              **
+ **  but WITHOUT ANY WARRANTY; without even the implied warranty of               **
+ **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                **
+ **  GNU General Public License for more details.                                 **
+ **                                                                               **
+ **  You should have received a copy of the GNU General Public License            **
+ **  along with this program; if not, write to the Free Software                  **
+ **  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.  **
+ **                                                                               **
+ ***********************************************************************************
+ ***********************************************************************************/
+
+
 #ifdef USING_MPI
 
 #include "mpi.hpp"
@@ -8,11 +32,10 @@
 
 #include <boost/test/unit_test.hpp>
 #include <boost/bind.hpp>
-                                  //  a namespace alias is preferred practice in real code
-namespace icr = institute_of_cancer_research;
+
 namespace mpi = boost::mpi;
 using namespace boost::unit_test;
-using namespace icr;
+using namespace ICR;
 //____________________________________________________________________________//
 
 class test_job{ 
